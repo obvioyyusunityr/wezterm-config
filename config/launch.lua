4a6620cceaa625089947ad67fecc74ffa4bfa1dd
obvioyyusunityr <linus.odhiambo@strathmore.edu>
2025-05-28 11:08:56 +0300

    modified:   launch.lua

--- config/launch.lua
+++ config/launch.lua
@@ -19,18 +19,18 @@ if platform.is_win then
       },
    }
 elseif platform.is_mac then
-   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+   options.default_prog = { '/opt/homebrew/bin/zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
+      { label = 'Fish', args = { '/opt/homebrew/bin/zsh', '-l' } },
       { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
 elseif platform.is_linux then
-   options.default_prog = { 'fish', '-l' }
+   options.default_prog = { 'zsh', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { 'fish', '-l' } },
+      { label = 'Fish', args = { 'zsh', '-l' } },
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
 end
